a4e2913499612a671483e2d57033e7ef6751b848
Anton Lunkov <a_lunkov@wargaming.net>
2024-03-04 21:43:28 +0100

    Fix cast-shadow and recieve-shadow, update lights properties

--- app.vue
+++ app.vue
@@ -21,6 +21,7 @@ onLoop(({ delta, elapsed }) => {
     <!-- <NuxtWelcome /> -->
     <TresCanvas
       clear-color="#82DBC5"
+      shadows
       window-size>
     <!-- Your scene here -->
       <TresPerspectiveCamera 
@@ -30,8 +31,8 @@ onLoop(({ delta, elapsed }) => {
       <TresMesh
         ref="boxRef"
         :scale="1"
-        :castShadow="true"
-        :recieveShadow="true">
+        cast-shadow
+        receive-shadow>
 
         <!-- <TresTorusGeometry :args="[1, 0.5, 16, 32]" /> -->
         <TresBoxGeometry :args="[1, 1, 1, 4, 4, 4]" />
@@ -43,8 +44,8 @@ onLoop(({ delta, elapsed }) => {
 
       <TresMesh 
         :position="[5, 0, 0]"
-        :castShadow="true"
-        :recieveShadow="true">
+        cast-shadow
+        receive-shadow>
         <TresSphereGeometry :args="[2, 32, 16, 0, 6.283185307179586, 0, 3.141592653589793]" />
         <!-- <TresMeshBasicMaterial color="orange" wireframe /> -->
         <TresMeshStandardMaterial color="orange" />
@@ -52,8 +53,8 @@ onLoop(({ delta, elapsed }) => {
 
       <TresMesh 
         :position="[0, 0, -2.5]"
-        :recieveShadow="true"
-        :castShadow="true">
+        receive-shadow
+        cast-shadow>
         <TresPlaneGeometry :args="[100, 100, 48, 48]" />
         <TresMeshStandardMaterial color="#ffffff" />
       </TresMesh>
@@ -61,9 +62,9 @@ onLoop(({ delta, elapsed }) => {
 
       <!-- <TresAmbientLight :intensity="1" /> -->
       <!-- <TresDirectionalLight color="#FFFFFF" :intensity="1" :shadow="true" :cast-shadow="true" :position="[0, 0, 10]"/> -->
-      <TresDirectionalLight ref="directionalLightRef" :args="['white', 1]" :position="[0, 0, 10]" v-light-helper/>
+      <TresDirectionalLight ref="directionalLightRef" :args="['white', 1]" :position="[0, 5, 15]" cast-shadow :shadow-mapSize-width="2048" :shadow-mapSize-height="2048" v-light-helper/>
 
-      <TresSpotLight color="#FFFFFF" :intensity="20" :distance="20" :decay="10" :position="[0, 20, 20]" :castShadow="true"/>
+      <TresSpotLight :args="['red', 1]" :distance="15" :decay="2" :penumbra="0.2" :position="[10, 5, 5]" :power="5000" cast-shadow :shadow-mapSize-width="4096" :shadow-mapSize-height="4096"  :shadow-camera-near="0.1" :shadow-camera-far="100" :shadow-radius="10" v-light-helper/>
 
       <OrbitControls />
     </TresCanvas>
